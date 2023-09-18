e2978ab828883988d6ba50b6c589b51ae6b1dce1
DockxGi <ginodockx@gmail.com>
2023-09-18 23:11:34 +0200

    add liquibase script to create word table

--- src/main/java/be/stadr/vikinglanguagecore/VikingLanguageCoreApplication.java
+++ src/main/java/be/stadr/vikinglanguagecore/VikingLanguageCoreApplication.java
@@ -1,8 +1,9 @@
 package be.stadr.vikinglanguagecore;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"be.stadr.vikinglanguagecore.rest_api"})
 public class VikingLanguageCoreApplication {
