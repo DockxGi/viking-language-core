a1f6c6afa2c79f2cb13001d0c7821688adf19e11
DockxGi <ginodockx@gmail.com>
2023-09-18 22:06:11 +0200

    implement minimal spring boot starter application

---
+++ src/main/java/com/example/demo/VikingLanguageCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingLanguageCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikingLanguageCoreApplication.class, args);
+	}
+
+}
