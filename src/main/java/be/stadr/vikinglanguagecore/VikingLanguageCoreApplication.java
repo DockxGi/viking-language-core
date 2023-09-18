122b5f442cf517540083eb2365e6034a0e514c02
DockxGi <ginodockx@gmail.com>
2023-09-18 22:25:35 +0200

    add ping endpoint that returns 200 OK response

--- src/main/java/be/stadr/vikinglanguagecore/VikingLanguageCoreApplication.java
+++ src/main/java/be/stadr/vikinglanguagecore/VikingLanguageCoreApplication.java
@@ -2,8 +2,9 @@ package be.stadr.vikinglanguagecore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"be.stadr.vikinglanguagecore.rest_api"})
 public class VikingLanguageCoreApplication {
 
 	public static void main(String[] args) {
